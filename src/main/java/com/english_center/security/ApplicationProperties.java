7bbafe556a35261c3aee58ec5e3648f14c6e20ab
Dinhhai2002 <88237144+Dinhhai2002@users.noreply.github.com>
2023-12-06 19:28:29 +0700

    update

---
+++ src/main/java/com/english_center/security/ApplicationProperties.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.english_center.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+@Getter
+@Component
+public class ApplicationProperties {
+
+	@Value("${base.url}")
+	private String baseUrl;
+
+	@Value("${key.driver}")
+	private String keyDriver;
+
+	@Value("${password.account.google}")
+	private String passwordAccountGoogle;
+
+	@Value("${key.folder.upload}")
+	private String folderUpload;
+
+}
